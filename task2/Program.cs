ff6f1800dce4f67366debd1b57ffd3d161be5cf4
alice <zhupanova2017sv@mail.ru>
2023-02-13 21:22:54 +0300

    sum of mun

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+while (num > 0)
+{
+    int count = num % 10;
+    result = result + count;
+    num = num / 10;
+}
+System.Console.WriteLine(result);
