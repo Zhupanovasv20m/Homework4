f4fa7e16efac047677e6c00302bbd87b9ba42a74
alice <zhupanova2017sv@mail.ru>
2023-02-15 17:59:12 +0300

    degree

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,10 +2,20 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+int Degree(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
 Console.WriteLine("Введите число А");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B");
 int B = Convert.ToInt32(Console.ReadLine());
 
-Double result = Math.Pow(A, B);
-System.Console.WriteLine($"{A}, {B} = {result}");
+int degree = Degree(A, B);
+System.Console.WriteLine($"{A}, {B} => {degree}");
